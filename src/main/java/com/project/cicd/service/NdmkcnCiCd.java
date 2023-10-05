8fe620c8331fc9f966e9e8b3e833ca35b4f980bd
Nedim Kaçan <nedimkacanofficial@gmail.com>
2023-10-05 14:49:52 +0300

    beyto methodu yazıldı

--- src/main/java/com/project/cicd/service/NdmkcnCiCd.java
+++ src/main/java/com/project/cicd/service/NdmkcnCiCd.java
@@ -5,4 +5,8 @@ public class NdmkcnCiCd {
     public void helloWorld() {
         System.out.println("Hello World!");
     }
+
+    public void beytoHello() {
+        System.out.println("Hello beyto hıyarı!");
+    }
 }
